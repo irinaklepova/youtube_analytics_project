e7aee8dd082cd158361e885783cf802e9d403591
Irina Klepova <ivklepova23@gmail.com>
2023-10-07 19:41:28 +0400

    реализованы атрибуты класса Channel, classmethod get_service и метод to_json

--- src/channel.py
+++ src/channel.py
@@ -5,21 +5,43 @@ from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        # в 15 строке я вообще не поняла, что произошлo, метод научного "пальцем в глаз")
-        self.info = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        self.info = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = self.info['items'][0]['snippet']['title']
         self.description = self.info['items'][0]['snippet']['description']
         self.url = self.info['items'][0]['snippet']['thumbnails']['default']['url']
-        self.subscriberCount = self.info['items'][0]['statistics']['subscriberCount']
-        self.videoCount = self.info['items'][0]['statistics']['videoCount']
-        self.viewCount = self.info['items'][0]['statistics']['viewCount']
+        self.subscriber_count = self.info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.info['items'][0]['statistics']['videoCount']
+        self.view_count = self.info['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.info, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        """Класс-метод, возвращающий объект для работы с YouTube API"""
+        api_key: str = os.getenv('YT_API_KEY')
+        return build('youtube', 'v3', developerKey=api_key)
+
+    def to_json(self, filename):
+        """Метод, сохраняющий в файл значения атрибутов экземпляра `Channel`"""
+        result = {
+            'id': self.__channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.view_count
+        }
+        file = open(filename, "w", encoding='utf-8')
+        json.dump(result, file)
+        file.close()
